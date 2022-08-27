52ee8b4e7d9023850067a01e2a0d11bd10541e8e
Md. Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2022-08-27 21:19:58 +0600

    file setup, and consoling

---
+++ JS Learn Start/01-Fundamentals/01-Console/app.js
@@ -0,0 +1,27 @@
+console.log("hi there")
+console.log('hi')
+console.log(123)
+console.log(true)
+console.log([12,23,3.3])
+var message = "it is a message";
+console.log(message)
+console.log({
+    name:"rakibul hassan rony",
+    age:"24"
+})
+console.table({
+    name:"rakibul hassan rony",
+    age:"24"
+})
+
+console.error("Error haha")
+console.warn("warning haha")
+console.clear()
+console.time("time start")
+console.log("1234")
+console.log("1234")
+console.log("1234")
+console.log("1234")
+console.log("1234")
+console.log("1234")
+console.timeEnd("time start")
